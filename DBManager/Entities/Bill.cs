a7a3d76795314e499455eee6a792c9a8582bd256
gonzalo <gonzalomedinamedina@gmail.com>
2023-10-29 20:00:07 +0900

    Created Entity base class and changed Bill class to inherits from Entity.

--- DBManager/Entities/Bill.cs
+++ DBManager/Entities/Bill.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
-
+﻿
 namespace DBManager.Entities
 {
 	public enum BillType
@@ -11,26 +10,24 @@ namespace DBManager.Entities
 		BILLS,
 		OTHERS
 	}
-	public class Bill
+
+	public class Bill : Entity
 	{
-		public string Id { get { return _id; } }
 		public string Name { get { return _name; } set { _name = value; } }
 		public int Price { get { return _price; } set { _price = value; } }
 		public BillType BillType { get { return _billType; } set { _billType= value; } }
 		public DateTime Date { get { return _date; } set { _date = value; } }
 
-		private string _id { get; set; }
 		private string _name { get; set; }
 		private int _price { get; set; }
 		private BillType _billType { get; set; }
 		private DateTime _date { get; set; }
 
-		public Bill() 
+		public Bill() : base(null)
 		{}
 
-		public Bill(string id, string name, int price, BillType billType, DateTime date)
+		public Bill(string id, string name, int price, BillType billType, DateTime date) : base(id)
 		{
-			_id = id;
 			_name = name;
 			_price = price;
 			_billType = billType;
---
+++ DBManager/Entities/Entity.cs
@@ -0,0 +1,13 @@
+﻿namespace DBManager.Entities
+{
+	public class Entity
+	{
+		public string Id { get { return _id; } }
+		private string _id { get; set; }
+
+		public Entity(string id)
+		{  
+			_id = id;
+		}
+	}
+}
