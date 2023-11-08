4a2ad02f62fe6cb61d1c070f36fb2029b5fd26fe
gonzalo <gonzalomedinamedina@gmail.com>
2023-11-08 13:43:45 +0900

    Added BillRepository to manage Bill entities.

---
+++ DBManager/Repository/IBillRepository.cs
@@ -0,0 +1,10 @@
+﻿using DBManager.Entities;
+
+namespace DBManager.Repository
+{
+    public interface IBillRepository
+    {
+        IList<Bill> GetBillsByDate(DateTime date);
+        IDictionary<DateTime, IList<Bill>> GetBillsByDateRange(DateTime dateFrom, DateTime dateTo);
+    }
+}
