cdd6b03605e032a8853c0f2129cdea8348e2e461
Gonzalo Medina Medina <gonzalomedinamedina@gmail.com>
2023-11-14 03:14:27 +0100

    Created BarChart and PieChart classes to generate respective configs.

---
+++ CountOn/BarChart.cs
@@ -0,0 +1,86 @@
+﻿
+using PSC.Blazor.Components.Chartjs.Models.Bar;
+using PSC.Blazor.Components.Chartjs.Models.Common;
+using CountOn.Repository;
+
+namespace CountOn
+{
+    public class BarChart
+    {
+        private static List<decimal?> GetYValues(IList<IBillDateSummary> billsByDate)
+        {
+            List<decimal?> bills = new List<decimal?>();
+            foreach (var billDateSummary in billsByDate)
+            {
+                bills.Add(billDateSummary.GetTotal());
+            }
+
+            return bills;
+        }
+
+        private static List<string> GetXLabels(IList<IBillDateSummary> billsByDate)
+        {
+            List<string> result = new List<string>();
+            foreach (var billDateSummary in billsByDate)
+            {
+                DateTime date = billDateSummary.GetDate();
+                result.Add(date.DayOfWeek.ToString() + "\n" + date.Day.ToString() + "/" + date.Month.ToString());
+            }
+
+            return result;
+        }
+    
+        public static BarChartConfig GetBarChartConfig(IList<IBillDateSummary> billsByDate, Func<CallbackGenericContext, ValueTask>? onClickAsync)
+        {
+            BarChartConfig _config = new BarChartConfig()
+            {
+                Options = new Options()
+                {
+                    Plugins = new Plugins()
+                    {
+                        Legend = new Legend()
+                        {
+                            Align = Align.Center,
+                            Display = false,
+                            Position = LegendPosition.Right
+                        }
+                    },
+                    Scales = new Dictionary<string, Axis>()
+                    {
+                        {
+                            Scales.XAxisId, new Axis()
+                            {
+                                Stacked = true,
+                                Ticks = new Ticks()
+                                {
+                                    MaxRotation = 0,
+                                    MinRotation = 0
+                                }
+                            }
+                        },
+                        {
+                            Scales.YAxisId, new Axis()
+                            {
+                                Stacked = true
+                            }
+                        }
+                    },
+                    OnClickAsync = onClickAsync
+                }
+            };
+
+            _config.Data.Labels = GetXLabels(billsByDate);
+            _config.Data.Datasets.Add(new BarDataset()
+            {
+                Label = "Value",
+                Data = GetYValues(billsByDate),
+                BackgroundColor = new List<string>() { "rgb(255, 0, 0)" },
+                BorderColor = new List<string>() { "rgb(0, 0, 0)" },
+                BorderWidth = 3,
+                Fill = true
+            });
+
+            return _config;
+        }
+    }
+}
---
+++ CountOn/PieChart.cs
@@ -0,0 +1,48 @@
+﻿using CountOn.Repository;
+using DBManager.Entities;
+using PSC.Blazor.Components.Chartjs.Models.Common;
+using PSC.Blazor.Components.Chartjs.Models.Pie;
+
+namespace CountOn
+{
+	public class PieChart
+	{
+		public static PieChartConfig GetPieChartConfig(IBillDateSummary billDateSummary)
+		{
+			PieChartConfig config = new PieChartConfig()
+			{
+				Options = new PieOptions()
+				{
+					Responsive = true,
+					Plugins = new Plugins()
+					{
+						Legend = new Legend()
+						{
+							Align = Align.Center,
+							Display = false,
+							Position = LegendPosition.Bottom
+						}
+					}
+				}
+			};
+
+			var billTypes = billDateSummary.GetBillTypes().ToList();
+			config.Data.Labels = billTypes.ConvertAll(x => x.ToString());
+			config.Data.Datasets.Add(new PieDataset()
+			{
+				Label = "Pie chart Label",
+				Data = billDateSummary.GetBillTypeValues(),
+				BackgroundColor = new List<string>() { "rgb(255, 0, 0)", "rgb(0, 255, 0)", "rgb(0, 0, 255)" },
+				BorderWidth = 3
+			});
+
+			return config;
+		}
+
+		private List<string> GetBillTypeColors(List<BillType> billTypes)
+		{
+			var colors = new List<string>();
+			return colors;
+		}
+	}
+}
